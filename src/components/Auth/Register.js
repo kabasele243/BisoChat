eb57874d72145c8fbf87092dcc03d76f71dc6005
Kabasele243 <fkabasele7@gmail.com>
2019-08-02 18:15:34 -0400

    Create Login Form and functionality

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -129,7 +129,8 @@ class Register extends React.Component {
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
+          <Header as="h1" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
             Register for BisoChat
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
