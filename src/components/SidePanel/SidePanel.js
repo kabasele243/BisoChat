90a13c79fa80af126793190bb5d79816acbb5e66
Kabasele243 <fkabasele7@gmail.com>
2019-08-08 14:42:55 -0400

    create directmessages component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,11 +1,14 @@
 import React from "react";
-import UserPanel from "./UserPanel";
 import { Menu } from "semantic-ui-react";
+
+import UserPanel from "./UserPanel";
 import Channels from "./Channels";
+import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component {
   render() {
     const { currentUser } = this.props;
+
     return (
       <Menu
         size="large"
@@ -16,6 +19,7 @@ class SidePanel extends React.Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
+        <DirectMessages />
       </Menu>
     );
   }
